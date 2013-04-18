0763b9cc80c008b0e085a7b6cf0b2d02a72a3eb7
Christoph Rumpel <c.rumpel@kabsi.at>
2013-04-18 22:01:14 +0200

    404 smile adaption

--- themes/cr2013/404.php
+++ themes/cr2013/404.php
@@ -1,7 +1,10 @@
 <?php get_header(); ?>
 
-
-<img class="404__icon" src="<?php bloginfo('template_directory'); ?>/_/img/smiley_sad.svg" alt="Smiley Sad">
-<h4>Ohh...seems like something went wrong here :-(</h4>
+<div class="grid-wrapper">
+	<div class="grid one-whole text--center">	
+		<img class="404__icon" src="<?php bloginfo('template_directory'); ?>/_/img/smiley_sad.svg" alt="Smiley Sad">
+		<h4>Ohh...seems like something went wrong here :-(</h4>
+	</div>
+</div>
 
 <?php get_footer(); ?>
