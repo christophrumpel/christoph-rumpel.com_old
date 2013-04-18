9b24bdf446a14cfbbc0cb541fbe62b374bd81264
Christoph Rumpel <c.rumpel@kabsi.at>
2013-04-18 21:57:50 +0200

    adding smiley icon 404 page

--- themes/cr2013/404.php
+++ themes/cr2013/404.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
 
+
+<img class="404__icon" src="<?php bloginfo('template_directory'); ?>/_/img/smiley_sad.svg" alt="Smiley Sad">
 <h4>Ohh...seems like something went wrong here :-(</h4>
 
 <?php get_footer(); ?>
