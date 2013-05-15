cbcf88205dba58a4592ebeb88534b76fa0efe94a
Christoph Rumpel <c.rumpel@kabsi.at>
2013-05-15 19:19:13 +0200

    strong getting different font

--- themes/cr2013/404.php
+++ themes/cr2013/404.php
@@ -3,7 +3,7 @@
 <div class="grid-wrapper">
 	<div class="grid one-whole text--center">	
 		<img class="404__icon" src="<?php bloginfo('template_directory'); ?>/_/img/smiley_sad.svg" alt="Smiley Sad">
-		<h4>Ohh...seems like something went wrong here :-(</h4>
+		<h4>Ohh...seems like something went wrong here...</h4>
 	</div>
 </div>
 
